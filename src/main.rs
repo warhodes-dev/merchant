a47274d143242dd292d51a43eb4809487e094ec0
warhodes-dev <warhodes@gmail.com>
2023-07-05 16:59:45 -0400

    switching to cursive ui

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use anyhow::Result;
-use cursive::{Cursive, reexports::log, views::Canvas, Printer, event::Event};
+use cursive::{Cursive, reexports::log, views::Canvas, Printer, event::{Event, EventResult}, view::Resizable, theme::{ColorStyle, Color}};
+
+mod world;
+use world::WorldApp;
 
 fn main() -> Result<()> {
     let backend_init = || -> std::io::Result<Box<dyn cursive::backend::Backend>> {
@@ -15,18 +18,41 @@ fn main() -> Result<()> {
     siv.add_global_callback('q', |s| s.quit());
     siv.add_global_callback('`', Cursive::toggle_debug_console);
 
-    let state = ();
-    let canvas = Canvas::new(state)
+    let worldapp = WorldApp::new((80, 40), 0);
+    let canvas = Canvas::new(worldapp.clone())
         .with_draw(draw)
-        .with_on_event(|state: &(), event| match event {
-            Event::Char('a) => {
-                let st = state;
+        .with_on_event(|worldapp: &mut WorldApp, event| match event {
+            Event::Char('a') => {
+                EventResult::consumed()
             }
-        });
+            _ => EventResult::Ignored
+        })
+        .fixed_size((worldapp.width, worldapp.height));
+
+    siv.add_layer(canvas);
+
+    siv.try_run_with(backend_init)?;
 
     Ok(())
 }
 
-fn draw(state: &(), printer: &Printer) {
-    log::info!("Drawing... Size: {}x{}", printer.size.x, printer.size.y);
+fn draw(worldapp: &WorldApp, p: &Printer) {
+    let canvas_width = p.size.x;
+    let canvas_height = p.size.y;
+
+    for x in 0..worldapp.width {
+        for y in 0..worldapp.height {
+            let tile = worldapp.get_tile(x, y);
+            let x1 = x as u8;
+            let y1 = y as u8;
+            let style = ColorStyle::new(
+                Color::Rgb(y1, x1, y1), // Why does this not work?
+                Color::Rgb(x1, y1, x1)
+            );
+            p.with_color(style, |printer| {
+                let mut buf = [0u8; 4];
+                printer.print( (x, y), tile.glyph().encode_utf8(&mut buf));
+            })
+        }
+    }
 }
--- src/world.rs
+++ src/world.rs
@@ -1,26 +1,47 @@
 use std::fmt::Write;
 use anyhow::Result;
-use crossterm::style::{
-    Stylize,
-    Color,
-};
+use cursive::theme::Color;
 use worldgen::{
     noise::perlin::PerlinNoise, 
     world::{Size, World, Tile, tile::{Constraint, ConstraintType}},
     noisemap::{NoiseMap, NoiseMapGenerator, NoiseMapGeneratorBase, Step, Seed}, constraint,
 };
 
+#[derive(Copy, Clone)]
+pub struct TileStyle {
+    glyph: char,
+    fg: (u8, u8, u8),
+    bg: (u8, u8, u8),
+}
+
+impl TileStyle {
+    pub fn fg_rgb(&self) -> Color {
+        Color::Rgb(self.fg.0, self.fg.1, self.fg.2)
+    }
+    pub fn bg_rgb(&self) -> Color {
+        Color::Rgb(self.bg.0, self.bg.1, self.bg.2)
+    }
+    pub fn glyph(&self) -> char { self.glyph }
+}
+
+#[derive(Clone)]
 pub struct WorldApp {
     seed: u32,
-    text: String,
+    tiles: Vec<TileStyle>,
+    pub width: usize,
+    pub height: usize,
 }
 
 impl WorldApp {
-    pub fn new() -> Self {
-        WorldApp {
-            seed: 0,
-            text: "Hello World".to_string()
-        }
+    pub fn new(size: (usize, usize), seed: u32) -> Self {
+        let mut world = WorldApp {
+            seed,
+            tiles: Vec::new(),
+            width: size.0,
+            height: size.1,
+        };
+        world.generate();
+        world
     }
 
     pub fn generate(&mut self) -> Result<()> {
@@ -33,30 +54,40 @@ impl WorldApp {
         );
 
         let world = World::new()
-            .set(Size::of(80, 50))
+            .set(Size::of(self.width as i64, self.height as i64))
             .add(Tile::new('~').when(constraint!(noisemap.clone(), < 0.0)))
             .add(Tile::new(',').when(constraint!(noisemap.clone(), < 0.45)))
             .add(Tile::new('^').when(constraint!(noisemap.clone(), > 0.8)))
             .add(Tile::new('n'));
 
-        let mut buf = String::new();
+        let mut tiles = Vec::new();
 
         for rows in world.generate(0, 0).iter() {
             for cols in rows.iter() {
                 for char in cols.iter() {
-                    write!(buf, "{char}")?;
+                    let tile = style_map(*char);
+                    tiles.push(tile);
                 }
-                writeln!(buf)?;
             }
         }
 
-        self.text = buf;
+        self.tiles = tiles;
         self.seed += 1;
 
         Ok(())
     }
 
-    pub fn text(&self) -> &str {
-        self.text.as_str()
+    pub fn get_tile(&self, x: usize, y: usize) -> TileStyle {
+        *self.tiles.get(y * self.width + x).expect(&format!("X:{x}, Y:{y}"))
+    }
+}
+
+fn style_map(c: char) -> TileStyle {
+    match c {
+        '~' => TileStyle{glyph: '~', fg: (1, 1, 1), bg: (0, 0, 255)},
+        ',' => TileStyle{glyph: ',', fg: (0, 0, 0), bg: (0, 255, 0)},
+        '^' => TileStyle{glyph: '^', fg: (0, 0, 0), bg: (100, 100, 100)},
+        'n' => TileStyle{glyph: 'n', fg: (0, 0, 0), bg: (99, 66, 33)},
+        _ => panic!("Unsupported tile character")
     }
 }
